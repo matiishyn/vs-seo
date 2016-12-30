2a8e47c00b64c9da58d7f894581c852bd242b93f
ozapotichny <miniraid@gmail.com>
2016-12-30 17:06:09 +0200

    Landing initialization

---
+++ src/+app/market/modules/landing/components/landing-product/landing-product.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'landing-product',
+    templateUrl: 'landing-product.component.html'
+})
+export class LandingProductComponent {
+    constructor() { }
+}
--- src/+app/market/modules/landing/landing.module.ts
+++ src/+app/market/modules/landing/landing.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { LandingComponent } from './landing.component';
 import { ImgHeaderComponent } from './components/img-header/img-header.component';
 import { SharedModule } from '../../../shared/shared.module';
+import { LandingProductComponent } from './components/landing-product/landing-product.component';
 
 @NgModule({
   imports: [SharedModule],
   exports: [],
-  declarations: [LandingComponent, ImgHeaderComponent],
+  declarations: [LandingComponent, ImgHeaderComponent, LandingProductComponent],
   providers: [],
 })
 export class LandingModule {
