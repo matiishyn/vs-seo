41abb91a796dd9dc64cbfc315ca030fdb7ace62f
ozapotichny <miniraid@gmail.com>
2016-12-30 14:48:12 +0200

    Translate selector

--- src/+app/main/main.component.ts
+++ src/+app/main/main.component.ts
@@ -2,14 +2,14 @@ import {Component, ChangeDetectionStrategy, ViewEncapsulation, OnInit} from '@an
 import {ActivatedRoute, Router, Params, UrlSegment, PRIMARY_OUTLET} from '@angular/router';
 import {TranslateService} from 'ng2-translate';
 import {UrlSegmentGroup} from "@angular/router/src/url_tree";
+import {isBrowser} from 'angular2-universal';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.Default,
   encapsulation: ViewEncapsulation.Emulated,
   selector: 'main',
   template: `<nav>
-    <a routerLinkActive='router-link-active' routerLink=''>Home</a>
-    <a routerLinkActive='router-link-active' routerLink='about'>About</a>
+    <a routerLinkActive='router-link-active' routerLink='./'>Home</a>
     <a routerLinkActive='router-link-active' routerLink='todo'>Todo</a>
     <a routerLinkActive='router-link-active' routerLink='lazy'>Lazy</a>
   </nav>
@@ -21,6 +21,8 @@ import {UrlSegmentGroup} from "@angular/router/src/url_tree";
 })
 
 export class MainComponent implements OnInit {
+  private language: string;
+
   constructor(private translate: TranslateService,
               private route: ActivatedRoute,
               private router: Router) {
@@ -29,13 +31,28 @@ export class MainComponent implements OnInit {
   changeLanguage(lang: string): void {
     const children: UrlSegmentGroup = this.router.parseUrl(this.router.url).root.children[PRIMARY_OUTLET];
     const s: UrlSegment[] = children.segments;
-    console.log(s);
-    this.router.navigate(['', lang], { relativeTo: this.route });
+    const paths = s.map(segment => segment.path);
+    paths.shift();
+    paths.unshift('', lang);
+    if (isBrowser) {
+      localStorage.setItem('VISALEX_LANG', lang);
+    }
+    this.router.navigate(paths, {relativeTo: this.route});
   }
 
   ngOnInit() {
+    if (isBrowser) {
+      const lang = localStorage.getItem('VISALEX_LANG');
+      if (lang) {
+        this.language = lang;
+        this.translate.use(lang);
+        this.changeLanguage(lang);
+        return;
+      }
+    }
     this.translate.setDefaultLang('en');
     this.route.params.subscribe(params => {
+      this.language = params['lang'];
       this.translate.use(params['lang']);
     });
   }
