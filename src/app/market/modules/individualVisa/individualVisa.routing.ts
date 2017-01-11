f0140267681c33fac5b72740f7c73e064b3551e3
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2017-01-11 17:35:36 +0100

    visa list separate route

--- src/app/market/modules/individualVisa/individualVisa.routing.ts
+++ src/app/market/modules/individualVisa/individualVisa.routing.ts
@@ -1,5 +1,5 @@
 import {Routes} from '@angular/router';
-import {IndividualVisaMainComponent} from "./individualVisaMain.component";
+import {IndividualVisaMainComponent} from "./individualVisaHowItWorks.component";
 import {IndividualVisaListComponent} from "./individualVisaList.component";
 
 export const individualVisaRoutes: Routes = [
