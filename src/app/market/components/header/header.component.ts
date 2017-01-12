1e9bde638ea6364a4687f5facd87558c48c6d740
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2017-01-12 09:29:45 +0100

    translations

--- src/app/market/components/header/header.component.ts
+++ src/app/market/components/header/header.component.ts
@@ -1,8 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, ChangeDetectionStrategy, ViewEncapsulation, OnInit} from '@angular/core';
+import {TranslateService} from 'ng2-translate';
+import {UrlSegmentGroup} from "@angular/router/src/url_tree";
+import {isBrowser, isNode} from 'angular2-universal';
 
 @Component({
   selector: 'my-header',
   templateUrl: 'header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  private language: string;
+
+  constructor(private translate: TranslateService) {
+  }
+
+  ngOnInit() {
+    /*if (isBrowser) {
+     const lang = localStorage.getItem('VISALEX_LANG');
+     if (lang) {
+     this.language = lang;
+     this.translate.use(lang);
+     this.changeLanguage(lang);
+     return;
+     }
+     }*/
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+    /*this.route.params.subscribe(params => {
+     this.language = params['lang'];
+     this.translate.use(params['lang']);
+     });*/
+  }
+
+  changeLanguage(lang: string): void {
+    /*this.translate.use(lang);
+     const children: UrlSegmentGroup = this.router.parseUrl(this.router.url).root.children[PRIMARY_OUTLET];
+     const s: UrlSegment[] = children.segments;
+     const paths = s.map(segment => segment.path);
+     paths.shift();
+     paths.unshift('', lang);
+     if (isBrowser) {
+     localStorage.setItem('VISALEX_LANG', lang);
+     }
+     this.router.navigate(paths, {relativeTo: this.route});*/
+  }
 }
--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -11,39 +11,6 @@ import {isBrowser} from 'angular2-universal';
   templateUrl: 'market.component.html'
 })
 
-export class MarketComponent implements OnInit {
-  private language: string;
+export class MarketComponent {
 
-  constructor(private translate: TranslateService) {
-  }
-
-  changeLanguage(lang: string): void {
-    /*this.translate.use(lang);
-    const children: UrlSegmentGroup = this.router.parseUrl(this.router.url).root.children[PRIMARY_OUTLET];
-    const s: UrlSegment[] = children.segments;
-    const paths = s.map(segment => segment.path);
-    paths.shift();
-    paths.unshift('', lang);
-    if (isBrowser) {
-      localStorage.setItem('VISALEX_LANG', lang);
-    }
-    this.router.navigate(paths, {relativeTo: this.route});*/
-  }
-
-  ngOnInit() {
-    /*if (isBrowser) {
-      const lang = localStorage.getItem('VISALEX_LANG');
-      if (lang) {
-        this.language = lang;
-        this.translate.use(lang);
-        this.changeLanguage(lang);
-        return;
-      }
-    }
-    this.translate.setDefaultLang('en');
-    this.route.params.subscribe(params => {
-      this.language = params['lang'];
-      this.translate.use(params['lang']);
-    });*/
-  }
 }
