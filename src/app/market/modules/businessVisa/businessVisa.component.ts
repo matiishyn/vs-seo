b94e2227d74f1117399766cff8e4ab347760f5d2
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2017-01-16 15:25:00 +0100

    router events

--- src/app/market/modules/businessVisa/businessVisa.component.ts
+++ src/app/market/modules/businessVisa/businessVisa.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
-    templateUrl: 'businessVisa.component.html'
+  templateUrl: 'businessVisa.component.html'
 })
-export class BusinessVisaComponent {
-    constructor() {}
+export class BusinessVisaComponent implements OnInit {
+  private headerVisibleRoutes: Array<string> = ['/business-visa/how-it-works', '/business-visa/list', '/business-visa'];
+  private isHeaderVisible: boolean;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(state => {
+      console.log(state.url);
+      this.isHeaderVisible = Boolean(this.headerVisibleRoutes.find(path => state.url.endsWith(path)));
+      console.log(this.isHeaderVisible);
+    });
+  }
 }
