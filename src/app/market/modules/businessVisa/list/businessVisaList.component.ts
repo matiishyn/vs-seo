3fe81ceb4ec82a7e56f3bd2e46093eb579f08e72
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2017-01-16 15:34:57 +0100

    business visa details page

--- src/app/market/modules/businessVisa/list/businessVisaList.component.ts
+++ src/app/market/modules/businessVisa/list/businessVisaList.component.ts
@@ -10,31 +10,31 @@ export class BusinessVisaListComponent {
       title: 'visaDetails_businessVisas_l-1a-visa_title',
       p: 'visaDetails_businessVisas_l-1a-visa_short_description',
       price: null,
-      link: []
+      link: ['../visa.l1a']
     }, {
       id: 'visa.o1',
       title: 'visaDetails_businessVisas_o-1-visa_title',
       p: 'visaDetails_businessVisas_o-1-visa_short_description',
       price: null,
-      link: []
+      link: ['../visa.o1']
     }, {
       id: 'visa.p1a',
       title: 'visaDetails_businessVisas_p-1a-visa_title',
       p: 'visaDetails_businessVisas_p-1a-visa_short_description',
       price: null,
-      link: []
+      link: ['../visa.p1a']
     }, {
       id: 'visa.h1b',
       title: 'visaDetails_businessVisas_h-1b-visa_title',
       p: 'visaDetails_businessVisas_h-1b-visa_short_description',
       price: null,
-      link: []
+      link: ['../visa.h1b']
     }, {
       id: 'visa.e2',
       title: 'visaDetails_businessVisas_e-2-visa_title',
       p: 'visaDetails_businessVisas_e-2-visa_short_description',
       price: null,
-      link: []
+      link: ['../visa.e2']
     },
   ];
 
