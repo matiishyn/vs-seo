e5f3af9ea371efcadeb9ae95f1182167351ab148
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2017-01-17 09:31:19 +0100

    b1b2 visa

--- src/app/market/components/visaDetails/visaDetails.data.ts
+++ src/app/market/components/visaDetails/visaDetails.data.ts
@@ -1,12 +1,14 @@
 export interface IVisasDetails {
-  [propName: string]:IVisaDetails
+  [propName: string]: IVisaDetails
 }
 export interface IVisaDetails {
   id: string;
-  // url: string;
   title: string;
   img: string;
   description: string;
+  priceAnnouncement: string;
+  price: string;
+  additionalPrice: string;
 }
 
 export const visaDetailsData: IVisasDetails = {
@@ -14,6 +16,9 @@ export const visaDetailsData: IVisasDetails = {
     id: 'visa.b1.b2',
     title: 'visaDetails_individualVisas_b1-b2-visa_title',
     img: '/assets/images/png/visa-detail-3.png',
-    description: 'visaDetails_individualVisas_b1-b2-visa_description'
+    description: 'visaDetails_individualVisas_b1-b2-visa_description',
+    priceAnnouncement: 'qualifying_processingFee_b1-b2-visa_priceAnnouncement',
+    price: 'qualifying_processingFee_b1-b2-visa_price',
+    additionalPrice: 'qualifying_processingFee_b1-b2-visa_additionalPrice'
   }
 };
